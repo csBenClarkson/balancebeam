c8a154b3dfd0961d9b1f457b4d42a3afc1c0be8f
BenClarkson <benclarkson@163.com>
2024-02-03 20:47:14 +0800

    :sparkles: Add port number arugument to benchmark server application.

--- benchmark/src/main.rs
+++ benchmark/src/main.rs
@@ -1,4 +1,6 @@
 use actix_web::{get, web, App, HttpServer, Responder};
+use std::env;
+use std::process::exit;
 
 #[get("/")]
 async fn index() -> impl Responder {
@@ -12,8 +14,17 @@ async fn hello(name: web::Path<String>) -> impl Responder {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 2 {
+        println!("Usage: {} <port>", args[0]);
+        exit(1);
+    }
+    let port = match args[1].parse::<u16>() {
+        Ok(p) => {p}
+        Err(_) => { println!("Invaild port."); exit(1); }
+    };
     HttpServer::new(|| App::new().service(index).service(hello))
-        .bind(("127.0.0.1", 9173))?
+        .bind(("127.0.0.1", port))?
         .run()
         .await
 }
